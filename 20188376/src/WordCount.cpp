3401c6cfa9b06b4fb304938555fc4ee52efe3f77
JingWenxing <511807280@qq.com>
2021-03-31 20:26:37 +0800

    全需求实现

--- 20188376/src/WordCount.cpp
+++ 20188376/src/WordCount.cpp
@@ -5,33 +5,63 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 // ********************************************************************************
 // 说明：			字符统计类
 // 
-// 属性：			vFileData					原始操作数据
-//												(数据为全ASCII字符的英文小写字符串)
+// 属性：			vFileData
+//					原始操作数据(数据为全ASCII字符的英文小写字符串)
 //
-// 操作：			StatisticsCharacter(string vFileData)
+//					vWordsList
+//					单词列表
+//
+//					vWordsListValue
+//					单词频率统计
+//
+// 公共函数：		StatisticsCharacter(string vFileData)
 //					复制构造函数，获取指定文件中的数据流
 //
 //					EffectiveRows()
 //					有效行数统计
+//
+//					EffectiveCharacter()
+//					有效字符数量统计
+//
+//					EffectiveWordsValue()
+//					有效单词数量统计
+//
+//					EffectiveWordFrequency()
+//					单词频率统计
+//
+// 私有函数：		EffectiveWords()
+//					有效单词统计
+//
+//					IsWord()
+//					单词有效判断
 // ********************************************************************************
 
 class StatisticsCharacter
 {
 private:
-	// 获得只保留汉字并全小写的数据串
 	string vFileData;
-
-public:
-	StatisticsCharacter(string vFileData);
+	vector<string> vWordsList;
+	vector<int> vWordsListValue;
 
 public:
 	int EffectiveRows();
+	int EffectiveCharacter();
+	int EffectiveWordsValue();
+	void EffectiveWordFrequency();
+
+private:
+	void EffectiveWords();
+	bool IsWord(string vTestWord);
+
+public:
+	StatisticsCharacter(string vFileData);
 };
 
 // ********************************************************************************
@@ -66,10 +96,34 @@ StatisticsCharacter::StatisticsCharacter(string vFilePath)
 
 		std::transform(vASCIIData.begin(), vASCIIData.end(), std::back_inserter(vFileData), std::tolower);
 	}
+	EffectiveWords();
 }
 
 // ********************************************************************************
-// 说明：			有效行数统计。这里使用流切割输入的方式，统计并储存包含了有效字符的
+// 说明：			统计文件ASCII字符的个数
+// ********************************************************************************
+
+int StatisticsCharacter::EffectiveCharacter()
+{
+	return vFileData.length();
+}
+
+// ********************************************************************************
+// 说明：			统计单词数量
+// ********************************************************************************
+
+int StatisticsCharacter::EffectiveWordsValue()
+{
+	int vTotal = 0;
+	for (int i = 0; i < vWordsListValue.size(); i++)
+	{
+		vTotal += vWordsListValue[i];
+	}
+	return vTotal;
+}
+
+// ********************************************************************************
+// 说明：			统计有效行数。这里使用流切割输入的方式，统计并储存包含了有效字符的
 //					行数。并输出其行数。
 // 
 // 操作变量：		oStreamFileData				文件操作流
@@ -93,9 +147,108 @@ int StatisticsCharacter::EffectiveRows()
 	return vRowsList.size();
 }
 
+// ********************************************************************************
+// 说明：			单词统计函数
+// 
+// 数据变量：		vsTemp						临时字符串
+// ********************************************************************************
+
+void StatisticsCharacter::EffectiveWords()
+{
+	string vsTemp = "";
+
+	for (int i = 0; i <= vFileData.length(); i++)
+	{
+		if (vFileData[i] >= 'a' && vFileData[i] <= 'z')
+			vsTemp += vFileData[i];
+		else
+		{
+			if (vFileData[i] >= '0' && vFileData[i] <= '9')
+				vsTemp += vFileData[i];
+			else
+			{
+				if (vsTemp.size() >= 4 && IsWord(vsTemp))
+				{
+					int flag = 0;
+					for (int j = 0; j < vWordsList.size(); j++)
+					{
+						if (vsTemp == vWordsList[j])
+						{
+							vWordsListValue[j]++;
+							flag = 1;
+							break;
+						}
+					}
+
+					if (flag == 0)
+					{
+						vWordsList.push_back(vsTemp);
+						vWordsListValue.push_back(1);
+					}
+					flag = 0;
+				}
+				vsTemp = "";
+			}
+		}
+	}
+}
+
+// ********************************************************************************
+// 说明：			单词首四位判断
+// ********************************************************************************
+
+bool StatisticsCharacter::IsWord(string vTestWord)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (vTestWord[i] >= '0' && vTestWord[i] <= '9')
+			return false;
+	}
+	return true;
+}
+
+typedef pair<string, int> PAIR;
+
+bool cmp_by_value(const PAIR& lhs, const PAIR& rhs) {
+	return lhs.second > rhs.second;
+}
+
+struct CmpByValue {
+	bool operator()(const PAIR& lhs, const PAIR& rhs) {
+		return lhs.second > rhs.second;
+	}
+};
+
+// ********************************************************************************
+// 说明：			单词频率统计。
+// 
+// 操作变量：		oWordFrequency				map键值对
+//
+// 数据变量：		vWordTop					频率最高的十个单词
+// ********************************************************************************
+
+void StatisticsCharacter::EffectiveWordFrequency()
+{
+	map<string, int> oWordFrequency;
+
+	for (int i = 0; i < vWordsList.size(); i++)
+		oWordFrequency.insert(pair<string, int>(vWordsList[i], vWordsListValue[i]));
+
+	vector<PAIR> vWordTop(oWordFrequency.begin(), oWordFrequency.end());
+	sort(vWordTop.begin(), vWordTop.end(), CmpByValue());
+	for (int i = 0; vWordTop.size() < 10 ? i != vWordTop.size() : i != 10; ++i) {
+		cout << vWordTop[i].first << ": " << vWordTop[i].second << endl;
+	}
+}
+
 int main()
 {
 	StatisticsCharacter vStatisticsCharacter("input.txt");
-	int vEffectiveRows = vStatisticsCharacter.EffectiveRows();
+
+	cout << "characters: " << vStatisticsCharacter.EffectiveCharacter() << endl;
+	cout << "words: " << vStatisticsCharacter.EffectiveWordsValue() << endl;
+	cout << "lines: " << vStatisticsCharacter.EffectiveRows() << endl;
+	vStatisticsCharacter.EffectiveWordFrequency();
+
 	return 0;
 }
